755af46b79992a21f968394ff3cdeefaa561d03c
CarterzCode <theotherdude42069@gmail.com>
2026-09-09 11:38:54 -0600

    Millfaersla

---
+++ Millifaersla.py
@@ -0,0 +1,30 @@
+"""
+Describe your program.
+First Last - Month Year
+"""
+
+def main() -> None:
+
+  # input
+  Monnei:int = int(input())
+  Fjee:int = int(input())
+  Dolladollabilljoll:int = int(input())
+  
+
+  # processing
+  # output
+
+  if Monnei < Fjee and Monnei < Dolladollabilljoll:
+    print("Monnei")
+  elif Fjee < Monnei and Monnei < Dolladollabilljoll:
+    print("Fjee")
+  else:
+    print("Dolladollabilljoll")
+  
+
+  
+
+
+if __name__ == "__main__":
+  main()
+    
