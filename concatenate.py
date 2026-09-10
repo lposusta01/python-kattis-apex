ba635bd06d3b37c730ec4446c08b4128cc0a4788
CarterzCode <theotherdude42069@gmail.com>
2026-09-10 09:27:20 -0600

    add concatenate program

---
+++ concatenate.py
@@ -0,0 +1,21 @@
+"""
+This concatenates two inputs
+Carter Quarles - September 2026
+"""
+
+def main() -> None:
+  
+
+  # input
+    input1 = input()
+    input2 = input()
+
+  # processing
+    combined = input1+input2
+  
+  # output
+    print(combined)
+
+if __name__ == "__main__":
+  main()
+    
