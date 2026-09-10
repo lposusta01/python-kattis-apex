f127a40bcac70c41f11c7515eae1f1eb3d6945ab
CarterzCode <theotherdude42069@gmail.com>
2026-09-10 09:46:21 -0600

    Add shortcuttowhat program

---
+++ shortcuttowhat.py
@@ -0,0 +1,19 @@
+"""
+Modifies and integer based on a given list of operations
+First Last - Month Year
+"""
+
+def main() -> None:
+
+  # input
+  n = int(input())
+
+  # processing
+  processed = (n+5)*3-10
+  
+  # output
+  print(processed)
+
+if __name__ == "__main__":
+  main()
+    
